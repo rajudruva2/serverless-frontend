ac7a51b649c1ce077e6827db6e82d61905d7f3b1
rajudruva2 <140049817+rajudruva2@users.noreply.github.com>
2025-03-20 11:53:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ import "./App.css";
 import jeansImage from "./assets/jeans.jpg"; 
 import tshirtImage from "./assets/tshirt.jpg"; 
 import sareeImage from "./assets/saree.jpg";
-import pf1 from "./assets/photo-frame-1.jpg";
+import pf1 from "./assets/photo-frame-1.jpg";import photo2h from "./assets/photo2h.jpg";
 
 const products = [
   { id: 1, name: "T-Shirt", price: 299, image: tshirtImage },
   { id: 2, name: "photo-frame1", price: 499, image: jeansImage },
   { id: 3, name: "yellow-Tshirt", price: 100, image: sareeImage },
-  { id: 3, name: "photo-frame2", price: 5000, image:pf1 },
+  { id: 5, name: "photo-frame2", price: 5000, image:pf1 },{ id: 4, name: "premium-dress", price: 1000000, image:photo2h },
 ];
 
 function App() {
